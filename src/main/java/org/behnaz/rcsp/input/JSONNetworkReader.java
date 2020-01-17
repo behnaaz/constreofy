ecbd6b305a30c84878cb680b75a628bccb5c83f9
changib <behnaz.changizi@rabobank.nl>
2020-01-17 20:59:13 +0100

    Refactoring

---
+++ src/main/java/org/behnaz/rcsp/input/JSONNetworkReader.java
@@ -0,0 +1,4 @@
+package org.behnaz.rcsp;
+
+public class JSONNetworkReader {
+}
---
+++ src/main/java/org/behnaz/rcsp/output/Drawer.java
@@ -0,0 +1,94 @@
+package org.behnaz.rcsp.output;
+
+import lombok.AllArgsConstructor;
+import org.behnaz.rcsp.GraphViz;
+import org.behnaz.rcsp.IOAwareSolution;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class Drawer {
+    private final String path;
+
+    final Map<String, String> labels = new HashMap<>();
+
+    public void draw(final List<IOAwareSolution> solutions) {
+        GraphViz gv = new GraphViz();
+        gv.addln(gv.start_graph());
+        for (IOAwareSolution sol : solutions) {
+            gv.addln(makeLine(sol));
+        }
+        gv.addln(gv.end_graph());
+        System.out.println(gv.getDotSource());
+
+        gv.increaseDpi();   // 106 dpi
+
+        String type = "svg";
+        //      String type = "dot";
+        //      String type = "fig";    // open with xfig
+        //      String type = "pdf";
+        //      String type = "ps";
+        //      String type = "svg";    // open with inkscape
+        //      String type = "png";
+        //      String type = "plain";
+
+        String repesentationType = "dot";
+        //		String repesentationType= "neato";
+        //		String repesentationType= "fdp";
+        //		String repesentationType= "sfdp";
+        // 		String repesentationType= "twopi";
+        // 		String repesentationType= "circo";
+
+        File out = new File( path + gv.getImageDpi() + "." + type);   // Linux
+        gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type, repesentationType), out);
+
+
+        final File desc = new File(path + "labels.txt");   // Linux
+        try {
+            FileOutputStream fos = new FileOutputStream(desc);
+            fos.write(labels.entrySet().stream().map(e -> e.getValue() + " = " + e.getKey()).collect(Collectors.joining("\n")).getBytes());
+            fos.close();
+        } catch (java.io.IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+
+    }
+
+    private String flow(final Set<String> flowVariables) {
+        return " [ label=\"" + cache(flowVariables) + " \" " + "]";
+    }
+
+
+    private String makeLine(final IOAwareSolution sol) {
+        String s = handleState(sol.getSolution().getFromVariables()).stream().map(e -> e.replaceAll("ring", "")).collect(Collectors.joining(",")) + " -> " + handleState(sol.getSolution().getToVariables()).stream().collect(Collectors.joining(",")).replaceAll("xring", "") + flow(sol.getSolution().getFlowVariables()) + ";";
+        System.out.println(s);
+        return s;
+    }
+
+    private String cache(final Set<String> flowVariables) {
+        final String lbl = flowVariables.isEmpty() ? "{}" : flowVariables.stream().collect(Collectors.joining(","));
+        if (labels.containsKey(lbl)) {
+            return labels.get(lbl);
+        }
+
+        String tmp = "L" + (labels.size() + 1);
+        labels.put(lbl, tmp);
+        return tmp;
+    }
+
+    private Set<String> handleState(final Set<String> set) {
+        if (set.isEmpty()) {
+            return new HashSet<>(Arrays.asList("empty"));
+        }
+        return set.stream().map(e -> e.replaceAll("\\d", "").substring(0, 2)).collect(Collectors.toSet());
+    }
+}
